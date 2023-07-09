13df20830f2bfa1118c84980f925a01c3e336b64
Russell Wallace <russell.wallace@gmail.com>
2023-07-09 03:17:09 +0100

    refactor

--- tools/fmt.bat
+++ tools/fmt.bat
@@ -1,33 +1,11 @@
-@echo off
 cd %~dp0..
 
 copy src %tmp% >nul:
 copy tools %tmp% >nul:
-copy example %tmp% >nul:
 
-clang-format -i --style=file src\*.h src\*.cc
-if %errorlevel% neq 0 goto :eof
-
-clang-format -i --style=file tools\*.h tools\*.cc
-if %errorlevel% neq 0 goto :eof
-
-clang-format -i --style=file example\*.h example\*.cc
-if %errorlevel% neq 0 goto :eof
-
-bin\sort-c src\*.h src\*.cc
-if %errorlevel% neq 0 goto :eof
-
-bin\sort-c tools\*.h tools\*.cc
-if %errorlevel% neq 0 goto :eof
-
-bin\sort-c example\*.h example\*.cc
-if %errorlevel% neq 0 goto :eof
-
-bin\sort-cases src\*.h src\*.cc
-if %errorlevel% neq 0 goto :eof
-
-bin\sort-cases tools\*.cc
-if %errorlevel% neq 0 goto :eof
-
-bin\sort-cases example\*.cc
-if %errorlevel% neq 0 goto :eof
+clang-format -i --style=file src\*.h src\*.cc||exit /b
+clang-format -i --style=file tools\*.h tools\*.cc||exit /b
+bin\sort-c src\*.h src\*.cc||exit /b
+bin\sort-c tools\*.h tools\*.cc||exit /b
+bin\sort-cases src\*.h src\*.cc||exit /b
+bin\sort-cases tools\*.cc||exit /b
