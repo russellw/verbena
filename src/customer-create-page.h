6516b92b0946e5de75fc128316d9bdb433c1d2cb
Russell Wallace <russell.wallace@gmail.com>
2023-08-10 11:14:32 +0100

    sidebar

--- src/customer-create-page.h
+++ src/customer-create-page.h
@@ -15,6 +15,8 @@ You should have received a copy of the GNU Affero General Public License along
 with Verbena.  If not, see <http:www.gnu.org/licenses/>.
 */
 
+#include "sidebar.h"
+
 html form {
 	html label {
 		"Name";
