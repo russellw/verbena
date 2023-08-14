5efdd89c4cb01aa9fa7c7fd22015e8db125e721b
Russell Wallace <russell.wallace@gmail.com>
2023-08-14 17:06:40 +0100

    rearrange

--- tools/fmt.bat
+++ tools/fmt.bat
@@ -1,12 +1,16 @@
 @echo off
 cd %~dp0..
 
+copy db %tmp% >nul:
 copy src %tmp% >nul:
 copy tools %tmp% >nul:
 
-clang-format -i --style=file src\*.h src\*.c*||exit /b
+clang-format -i --style=file db\*.h db\*.cc||exit /b
+clang-format -i --style=file src\*.h src\*.cc||exit /b
 clang-format -i --style=file tools\*.h tools\*.cc||exit /b
-bin\sort-c src\*.h src\*.c*||exit /b
+bin\sort-c db\*.h db\*.cc||exit /b
+bin\sort-c src\*.h src\*.cc||exit /b
 bin\sort-c tools\*.h tools\*.cc||exit /b
-bin\sort-cases src\*.h src\*.c*||exit /b
+bin\sort-cases db\*.h db\*.cc||exit /b
+bin\sort-cases src\*.h src\*.cc||exit /b
 bin\sort-cases tools\*.h tools\*.cc||exit /b
