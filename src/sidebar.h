d443b606bbe553069c9c76a70717497ffff156c4
Russell Wallace <russell.wallace@gmail.com>
2023-08-20 14:17:19 +0100

    color

--- src/sidebar.h
+++ src/sidebar.h
@@ -5,15 +5,18 @@ head {
 	}
 	style {
 		body {
+			// SORT
 			display flex;
 			'font-family' 'Arial,sans-serif';
 			'font-size' 20px;
 		}
 		table {
+			// SORT
 			'border-collapse' collapse;
 			width '100%';
 		}
 		th, td {
+			// SORT
 			border '1px solid #d3d3d3';
 			padding 8px;
 			'text-align' left;
@@ -23,10 +26,16 @@ head {
 		}
 	}
 }
+
+// body does not need a closing tag
 '<body>';
+
+// sidebar
 div {
 	@style {
-		'background-color' 'lightgray';
+		// SORT
+		'background-color' '#000000';
+		color '#ffffff';
 		padding 20px;
 	}
 	h2 "Sales";
