3986dab0fd923a03f4695c91888b483594ba9f95
Russell Wallace <russell.wallace@gmail.com>
2023-08-21 00:14:57 +0100

    simplify

--- tools/tools.h
+++ tools/tools.h
@@ -111,10 +111,7 @@ bool endsWith(const string& s, const char* t) {
 	auto n = strlen(t);
 	if (s.size() < n)
 		return 0;
-	for (auto i = 0; i < n; ++i)
-		if (s[s.size() - n + i] != t[i])
-			return 0;
-	return 1;
+	return memcmp(s.data() + s.size() - n, t, n) == 0;
 }
 
 string esc(const string& s) {
@@ -178,8 +175,5 @@ bool startsWith(const string& s, const char* t) {
 	auto n = strlen(t);
 	if (s.size() < n)
 		return 0;
-	for (auto i = 0; i < n; ++i)
-		if (s[i] != t[i])
-			return 0;
-	return 1;
+	return memcmp(s.data(), t, n) == 0;
 }
