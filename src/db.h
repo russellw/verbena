6f9acaca6a89b9594584acda5c3f1d77ecc196f8
Russell Wallace <russell.wallace@gmail.com>
2023-10-09 21:49:47 +0100

    for-eacb

--- src/db.h
+++ src/db.h
@@ -34,6 +34,34 @@ public:
 		return !n;
 	}
 
+	struct Iterator {
+		int i;
+
+		Iterator(int i): i(i) {
+		}
+
+		int operator*() {
+			return i;
+		}
+
+		Iterator& operator++() {
+			++i;
+			return *this;
+		}
+
+		bool operator!=(Iterator b) const {
+			return i != b.i;
+		}
+	};
+
+	Iterator begin() {
+		return Iterator(0);
+	}
+
+	Iterator end() {
+		return Iterator(n);
+	}
+
 	char* operator()(int i, int j) {
 		return PQgetvalue(r, i, j);
 	}
