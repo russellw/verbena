a54e484a1461985012cf626d3c5d2bd1b668b557
Russell Wallace <russell.wallace@gmail.com>
2023-10-11 07:34:44 +0100

    sort inplace

--- tools/fmt.bat
+++ tools/fmt.bat
@@ -8,9 +8,9 @@ copy tools %tmp% >nul:
 clang-format -i --style=file db\*.h db\*.cc||exit /b
 clang-format -i --style=file src\*.h src\*.cc||exit /b
 clang-format -i --style=file tools\*.h tools\*.cc||exit /b
-sort-c db\*.h db\*.cc||exit /b
-sort-c src\*.h src\*.cc||exit /b
-sort-c tools\*.h tools\*.cc||exit /b
-sort-cases db\*.h db\*.cc||exit /b
-sort-cases src\*.h src\*.cc||exit /b
-sort-cases tools\*.h tools\*.cc||exit /b
+sort-c -i db\*.h db\*.cc||exit /b
+sort-c -i src\*.h src\*.cc||exit /b
+sort-c -i tools\*.h tools\*.cc||exit /b
+sort-cases -i db\*.h db\*.cc||exit /b
+sort-cases -i src\*.h src\*.cc||exit /b
+sort-cases -i tools\*.h tools\*.cc||exit /b
