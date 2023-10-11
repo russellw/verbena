cb49e628f9e8e3bb679401681fa8253942438744
Russell Wallace <russell.wallace@gmail.com>
2023-10-11 01:48:13 +0100

    Create database directly from SQL

---
+++ src/schema.sql
@@ -0,0 +1,35 @@
+\set ON_ERROR_STOP on
+CREATE DATABASE verbena TEMPLATE template0 ENCODING 'UTF8' LOCALE 'en_US.UTF-8';
+\c verbena
+CREATE TABLE country(
+	code TEXT PRIMARY KEY,
+	name TEXT
+);
+CREATE TABLE customer(
+	id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name TEXT,
+	email TEXT,
+	phone TEXT,
+	delivery_address TEXT,
+	billing_address TEXT
+);
+CREATE TABLE estimate(
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	customer INTEGER NOT NULL REFERENCES customer(id),
+	date DATE NOT NULL,
+	expires DATE
+);
+CREATE TABLE product(
+	id TEXT PRIMARY KEY,
+	name TEXT,
+	cost DECIMAL,
+	price DECIMAL
+);
+CREATE TABLE estimate_line(
+	id BIGINT NOT NULL REFERENCES estimate(id),
+	line SMALLINT NOT NULL,
+	product TEXT REFERENCES product(id),
+	description TEXT,
+	qty DECIMAL,
+	price DECIMAL
+);
