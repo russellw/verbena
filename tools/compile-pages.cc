a77580f300cd69eff440950f1eb2a65d59b10236
Russell Wallace <russell.wallace@gmail.com>
2023-10-18 03:05:57 +0100

    refactor

--- tools/compile-pages.cc
+++ tools/compile-pages.cc
@@ -15,7 +15,74 @@ You should have received a copy of the GNU Affero General Public License along
 with Verbena.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "all.h"
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <algorithm>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+using filesystem::path;
+
+#ifdef NDEBUG
+#define debug(a)
+#else
+#define debug(a) cout << __FILE__ << ':' << __LINE__ << ": " << __func__ << ": " << #a << ": " << (a) << '\n'
+#endif
+
+// input
+string file;
+string text;
+
+void readText() {
+	ifstream is(file, ios::in);
+	text = {istreambuf_iterator<char>(is), istreambuf_iterator<char>()};
+
+	// make sure input ends with a newline, to simplify parser code
+	if (text.empty() || text.back() != '\n')
+		text += '\n';
+}
+
+// SORT
+bool eq(const char* s, const char* t) {
+	for (auto i = strlen(t); i--;)
+		if (*s++ != *t++)
+			return 0;
+	return 1;
+}
+
+string esc(string s) {
+	string o = "\"";
+	for (auto c: s) {
+		if (isprint((unsigned char)c)) {
+			if (c == '"')
+				o += '\\';
+			o += c;
+			continue;
+		}
+		char buf[7];
+		sprintf_s(buf,sizeof buf, "\\x%02x\"\"", (unsigned char)c);
+		o += buf;
+	}
+	return o + '"';
+}
 
 // SORT
 string camelCase(string s) {
