00507bdebea8a6634c1ca1b6e294f8bf9ca49a61
Russell Wallace <russell.wallace@gmail.com>
2025-03-10 16:09:09 +0000

    Modified 1 file(s): src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,21 @@ use vm::*;
 
 fn main() {
     let mut code = Vec::<Inst>::new();
+
     code.push(Inst::Const(Val::Num(dec256!(1))));
     code.push(Inst::Const(Val::Num(dec256!(2))));
     code.push(Inst::Add);
     code.push(Inst::Print);
     code.push(Inst::Const(Val::string("\n")));
     code.push(Inst::Print);
+
+    code.push(Inst::Const(Val::string("red and ")));
+    code.push(Inst::Const(Val::string("green = yellow")));
+    code.push(Inst::Add);
+    code.push(Inst::Print);
+    code.push(Inst::Const(Val::string("\n")));
+    code.push(Inst::Print);
+
     code.push(Inst::End);
     let mut vm = VM::new(code);
     if let Err(err_msg) = vm.run() {
