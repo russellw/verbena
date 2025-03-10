0db856f8e2240226670c1ede699a75d96a7cbd79
Russell Wallace <russell.wallace@gmail.com>
2025-03-10 17:06:14 +0000

    Added 1 file(s): src/parser.rs; Modified 1 file(s): src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
-use fastnum::dec256;
-
 mod vm;
 use vm::*;
 
+mod parser;
+use parser::*;
+
 fn main() {
-    let mut code = Vec::<Inst>::new();
-
-    code.push(Inst::Const(Val::Num(dec256!(1))));
-    code.push(Inst::Const(Val::Num(dec256!(2))));
-    code.push(Inst::Add);
-    code.push(Inst::Print);
-    code.push(Inst::Const(Val::string("\n")));
-    code.push(Inst::Print);
-
-    code.push(Inst::Const(Val::string("red and ")));
-    code.push(Inst::Const(Val::string("green = yellow")));
-    code.push(Inst::Add);
-    code.push(Inst::Print);
-    code.push(Inst::Const(Val::string("\n")));
-    code.push(Inst::Print);
-
-    code.push(Inst::End);
+	let  text="42";
+    let code = parse(text);
     let mut vm = VM::new(code);
     if let Err(err_msg) = vm.run() {
         eprintln!("{}", err_msg);
---
+++ src/parser.rs
@@ -0,0 +1,30 @@
+mod vm;
+use vm::*;
+
+struct Parser<'a> {
+    text: &'a str,
+    pos: usize,
+     code : Vec<Inst>,
+}
+
+impl<'a> Parser<'a> {
+    fn new(text: &'a str) -> Self {
+        Parser {
+            text,
+            pos: 0,
+     code : Vec::<Inst>::new(),
+        }
+    }
+    
+    fn expr(&mut self) -> Result<(), String> {
+    }
+    
+    fn parse(&mut self) -> Result<Vec<Inst>, String> {
+		expr();
+    }
+}
+
+pub fn parse(text: &str) -> Result<Vec<Inst>, String> {
+    let mut parser = Parser::new(text);
+    parser.parse()
+}
