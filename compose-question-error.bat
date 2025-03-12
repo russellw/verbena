c091f2b61f9e06f18d5fd44a2157d13437dbb8e6
Russell Wallace <russell.wallace@gmail.com>
2025-03-12 22:51:13 +0000

    Modified 1 file(s): compose-question-error.bat

--- compose-question-error.bat
+++ compose-question-error.bat
@@ -1,6 +1,6 @@
-echo The following code is getting this error:>\t\post.txt
-cargo test 2>>\t\post.txt
-echo ```>>\t\post.txt
-type %1 >>\t\post.txt
-echo ```>>\t\post.txt
-clip <\t\post.txt
+echo The following code is getting this error:>\t\q.txt
+cargo test 2>>\t\q.txt
+echo ```>>\t\q.txt
+type %1 >>\t\q.txt
+echo ```>>\t\q.txt
+clip <\t\q.txt
