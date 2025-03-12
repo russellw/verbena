e582b8f21d124e025c062d393cf8e3408aee516e
Russell Wallace <russell.wallace@gmail.com>
2025-03-12 03:59:06 +0000

    Modified 1 file(s): tests/empty.rs

--- tests/empty.rs
+++ tests/empty.rs
@@ -1,8 +1,36 @@
 use verbena::*;
 
 #[test]
-fn test() {
+fn empty() {
     let text = "";
+    let r = parse(text);
+    assert!(r.is_ok());
+}
+
+#[test]
+fn space() {
+    let text = " ";
+    let r = parse(text);
+    assert!(r.is_ok());
+}
+
+#[test]
+fn tab() {
+    let text = "\t";
+    let r = parse(text);
+    assert!(r.is_ok());
+}
+
+#[test]
+fn blank_line() {
+    let text = "\n";
+    let r = parse(text);
+    assert!(r.is_ok());
+}
+
+#[test]
+fn cr() {
+    let text = "\r";
     let r = parse(text);
     assert!(r.is_ok());
 }
