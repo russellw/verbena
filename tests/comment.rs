d7dcef2ef368d1a0e6effc726bfd303ac4b00f8a
Russell Wallace <russell.wallace@gmail.com>
2025-03-12 03:57:38 +0000

    Modified 1 file(s): tests/comment.rs

--- tests/comment.rs
+++ tests/comment.rs
@@ -1,8 +1,29 @@
 use verbena::*;
 
 #[test]
-fn test() {
+fn test_rem() {
     let text = "REM No code here";
+    let r = parse(text);
+    assert!(r.is_ok());
+}
+
+#[test]
+fn test_rem_title_case() {
+    let text = "Rem Or here";
+    let r = parse(text);
+    assert!(r.is_ok());
+}
+
+#[test]
+fn test_single_quote() {
+    let text = "' No code here";
+    let r = parse(text);
+    assert!(r.is_ok());
+}
+
+#[test]
+fn test_shebang() {
+    let text = "#! No code here";
     let r = parse(text);
     assert!(r.is_ok());
 }
