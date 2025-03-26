20153bd2a3b2a8e399ab4d0dda7bce3c98188909
Russell Wallace <russell.wallace@gmail.com>
2025-03-26 08:11:59 +0000

    Modified 1 file(s): emit_llvm.bat

--- emit_llvm.bat
+++ emit_llvm.bat
@@ -2,4 +2,5 @@ set RUSTFLAGS=--emit=llvm-ir
 cargo build --release||exit /b
 set RUSTFLAGS=
 wc target\debug\deps\verbena.ll
-copy target\debug\deps\verbena.ll \t\1.ll
+move \t\9.ll \t\8.ll
+copy target\debug\deps\verbena.ll \t\9.ll
