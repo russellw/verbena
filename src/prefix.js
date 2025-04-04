7e91a52636cb9c077fe45d0059499f6dba5d9aa2
Russell Wallace <russell.wallace@gmail.com>
2025-04-04 12:45:53 +0100

    Modified 1 file(s): src/prefix.js

--- src/prefix.js
+++ src/prefix.js
@@ -8,6 +8,26 @@ const min=Math.min;
 const max=Math.max;
 const sqrt=Math.sqrt;
 
+function _get(a,k){
+	if(a instanceof Map)
+		return  a.get(k);
+	return a[k];
+}
+
+function _set(a,k,x){
+	if(a instanceof Map)
+		a.set(k,x);
+	else
+		a[k]=x;
+	return x
+}
+
+function len(a){
+	if(a instanceof Map)
+		return  a.size;
+	return a.length
+}
+
 function _prin(a){
 	process.stdout.write(a.toString())
 }
