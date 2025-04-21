8ecb8060695bf5219dff24c4cac13fa99504e4d9
Russell Wallace <russell.wallace@gmail.com>
2025-04-21 19:25:45 +0100

    Modified 1 file(s): boot/parser.js

--- boot/parser.js
+++ boot/parser.js
@@ -22,12 +22,36 @@ function lex() {
 		const i = pos
 		const c = txt[pos]
 		switch (c) {
+			case '"':
+			case "'":
+				pos++
+				while (txt[pos] !== c) {
+					switch (txt[pos]) {
+						case "\n":
+							err("Unterminated string")
+							break
+						case "\\":
+							// Backslash can escape many things
+							// but most of them can be left to the JavaScript compiler to interpret
+							// The only things we need to worry about here are:
+							// Escaping a closing quote
+							// Escaping a backslash that might otherwise escape a closing quote
+							if (txt[pos] === c || txt[pos] === "\\") {
+								pos++
+							}
+							break
+					}
+					pos++
+				}
+				pos++
+				tok = txt.slice(i, pos)
+				return
 			case "#":
 			case "\n":
 				// End of line is a token
 				// but, to simplify the parser, blank lines are not tokens
 				while (pos < txt.length) {
-					let c = txt[pos]
+					const c = txt[pos]
 					if (/\s/.test(c)) {
 						if (c === "\n") {
 							line++
@@ -109,4 +133,8 @@ function isIdStart(c) {
 
 function isIdPart(c) {
 	return /[a-zA-Z0-9_$]/.test(c)
+}
+
+function err(msg) {
+	throw `{file}:{line}: {msg}`
 }
