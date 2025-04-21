da357f157710e1ee8fe963d04e2f2eca3a76b263
Russell Wallace <russell.wallace@gmail.com>
2025-04-21 07:57:58 +0100

    Modified 1 file(s): boot/parser.js

--- boot/parser.js
+++ boot/parser.js
@@ -2,11 +2,36 @@
 
 import { readFileSync } from "fs"
 
+const eof=' '
+
 let file
 let txt
+let ti=0
+let line=1
+let tok
+
+function lex(){
+	while(ti<txt.length){
+		const c=txt[ti]
+		switch(c){
+			case'\n':
+			line++
+			ti++
+			continue
+		}
+		if  (/\s/.test(c)){
+			ti++
+			continue
+		}
+		tok=c
+		ti++
+		return
+	}
+	tok=eof
+}
 
 export function parse(file1) {
 	file = file1
 	txt = readFileSync(file, "utf8") + "\n"
-	console.log(txt)
+	lex()
 }
