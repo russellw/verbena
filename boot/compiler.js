e92008ed2beb76b62368708e47fced41155fb2e1
Russell Wallace <russell.wallace@gmail.com>
2025-04-22 12:30:40 +0100

    Modified 1 file(s): boot/compiler.js

--- boot/compiler.js
+++ boot/compiler.js
@@ -8,6 +8,33 @@ function emit(s) {
 	txt += s
 }
 
+// Expressions
+function expr(a) {
+	if (typeof a === "string") {
+		emit(a)
+		return
+	}
+	if (/\w/.test(a.op[0])) {
+		emit(a.op)
+		emit("(")
+		emit(")")
+		return
+	}
+}
+
+// Statements
+function stmt(a) {
+	switch (a.op) {
+		case "import":
+			emit(`import ${a.v[0]} from "./${a.v[0]}.js"`)
+			break
+		default:
+			expr(a)
+			break
+	}
+	emit("\n")
+}
+
 // Top level
 export function compile(file, v) {
 	writeFileSync(file, txt, "utf8")
