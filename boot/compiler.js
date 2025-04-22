731d090e61c688d6889052d97da2baa5f35a5549
Russell Wallace <russell.wallace@gmail.com>
2025-04-23 00:01:13 +0100

    Modified 1 file(s): boot/compiler.js

--- boot/compiler.js
+++ boot/compiler.js
@@ -8,76 +8,85 @@ function emit(s) {
 	txt += s
 }
 
-// Expressions
-function expr(a) {
-	// Atom
-	if (typeof a === "string") {
-		emit(a)
-		return
-	}
-
-	// Function call
-	if (/\w/.test(a.op[0])) {
-		emit(a.op)
-		emit("(")
-		commaSeparated(a.v)
-		emit(")")
-		return
-	}
-
-	// Prefix
-	if (a.v.length === 1) {
-		emit("(")
-		emit(a.op)
-		expr(a.v[0])
-		emit(")")
-		return
-	}
-
-	// Infix
-	if (a.v.length === 2) {
-		emit("(")
-		expr(a.v[0])
-		emit(a.op)
-		expr(a.v[1])
-		emit(")")
-		return
-	}
-
-	throw a
-}
-
-function commaSeparated(v) {
-	for (let i = 0; i < v.length; i++) {
-		if (i) {
-			emit(",")
+function fn(params, body, topLevel) {
+	// Expressions
+	function expr(a) {
+		// Atom
+		if (typeof a === "string") {
+			emit(a)
+			return
 		}
-		expr(v[i])
-	}
-}
 
-// Statements
-function stmt(a, last) {
-	switch (a.op) {
-		case "import":
-			emit(`import ${a.v[0]} from "./${a.v[0]}.js"`)
-			break
-		default:
-			expr(a)
-			break
-	}
-	emit("\n")
-}
+		// Function call
+		if (/\w/.test(a.op[0])) {
+			emit(a.op)
+			emit("(")
+			commaSeparated(a.v)
+			emit(")")
+			return
+		}
 
-function block(v, last) {
-	for (let i = 0; i < v.length - 1; i++) {
-		stmt(v[i])
+		// Prefix
+		if (a.v.length === 1) {
+			emit("(")
+			emit(a.op)
+			expr(a.v[0])
+			emit(")")
+			return
+		}
+
+		// Infix
+		if (a.v.length === 2) {
+			emit("(")
+			expr(a.v[0])
+			emit(a.op)
+			expr(a.v[1])
+			emit(")")
+			return
+		}
+
+		throw a
 	}
-	stmt(v.at(-1), true)
+
+	function commaSeparated(v) {
+		for (let i = 0; i < v.length; i++) {
+			if (i) {
+				emit(",")
+			}
+			expr(v[i])
+		}
+	}
+
+	// Statements
+	function stmt(a, last) {
+		switch (a.op) {
+			case "import":
+				emit(`import ${a.v[0]} from "./${a.v[0]}.js"`)
+				break
+			default:
+				expr(a)
+				break
+		}
+		emit("\n")
+	}
+
+	function block(v, last) {
+		for (let i = 0; i < v.length; i++) {
+			stmt(v[i], i === v.length - 1)
+		}
+	}
+
+	// Normalize parameters
+	for (const a of params) {
+		emit(`if (${a} === undefined) ${a} = null\n`)
+	}
+
+	// Generate code
+	block(body, true)
 }
 
 // Top level
 export function compile(file, v) {
-	block(v)
+	fn([], v, true)
 	fs.writeFileSync(file, txt, "utf8")
 }
