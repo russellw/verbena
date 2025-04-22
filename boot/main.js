e1c15748f494f85f29b78cc78305109bb82caf33
Russell Wallace <russell.wallace@gmail.com>
2025-04-22 11:43:49 +0100

    Modified 1 file(s): boot/main.js

--- boot/main.js
+++ boot/main.js
@@ -1,13 +1,27 @@
 "use strict"
 
 import path from "path"
+import util from "util"
 
 import { parse } from "./parser.js"
 import { compile } from "./compiler.js"
 
-const args = process.argv.slice(2)
-const file = args[0]
+// Command line
+const file = process.argv[2]
+
+// Parse
 const v = parse(file)
-console.log(v)
+console.log(
+	util.inspect(v, {
+		showHidden: false,
+		depth: null,
+		colors: true,
+		maxArrayLength: null,
+		maxStringLength: null,
+		compact: true,
+	}),
+)
+
+// Compile
 const name = path.basename(file, path.extname(file))
 compile(`build/${name}.js`, v)
